40b2c9857c2d7d8ca77962b445c758537795b21d
alldakr <alldakr@gmail.com>
2023-10-17 19:49:54 +0900

    added vite

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import {} from "vite";
+
+const viteConfig = defineConfig({
+    server: {
+        host: true,
+        port: 3000
+    }
+});
+
+export default viteConfig;
